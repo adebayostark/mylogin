2358702a55644ac510debe6fb329459a5cd7262c
AdeStark <23214483+adebayostark@users.noreply.github.com>
2019-11-06 09:49:25 +0100

    second commit

--- routes.py
+++ routes.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def operator():
+def load_user():
     return render_template('companyLogin.html')
 
+@app.route('/us/authwall')
+def load_staff():
+    return render_template('staffLogin.html')
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="localhost", port=8001, debug=True)
